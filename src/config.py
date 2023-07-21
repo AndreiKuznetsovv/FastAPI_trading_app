ca845173f5336fd6301c425687e2b5747996b9dc
Andrei <yeloww51@gmail.com>
2023-07-21 10:28:02 +0300

    Application structure

---
+++ src/config.py
@@ -0,0 +1,15 @@
+from os import path, environ
+
+from dotenv import load_dotenv
+
+"""App configuration"""
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '../.env'))
+
+class Config:
+    DB_HOST = environ.get("DB_HOST")
+    DB_PORT = environ.get("DB_PORT")
+    DB_NAME = environ.get("DB_NAME")
+    DB_USER = environ.get("DB_USER")
+    DB_PASS = environ.get("DB_PASS")
